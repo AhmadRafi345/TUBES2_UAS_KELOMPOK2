b4c9577c7bfcc046ee403f6c7f00b9fc0b8a82c8
Achmad Roychan <achmadroychan501@gmail.com>
2024-12-21 22:58:09 +0700

    convert add vote json to model class ( coding ) ada 2 yaitu request body dan responses

---
+++ tubes2_uas_kelompok2/lib/data/vote/requestsmodel/vote_requests_model.dart
@@ -0,0 +1,12 @@
+class VoteRequestsModel {
+  final String imageId;
+  final String subId;
+  final int value;
+
+  VoteRequestsModel(
+      {required this.imageId, required this.subId, required this.value});
+
+  Map<String, dynamic> toJson() {
+    return {"image_id": "asf2", "sub_id": "my-user-1234", "value": 1};
+  }
+}
