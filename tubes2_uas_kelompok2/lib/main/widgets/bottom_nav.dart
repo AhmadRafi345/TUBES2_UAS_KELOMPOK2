0845e6b50adbf2f45cd31a12c7fe3654a0e0283a
Tia <Setianingrum005@gmai.com>
2024-12-29 22:01:35 +0700

    feat : layouting main_menu_screen ( coding )

---
+++ tubes2_uas_kelompok2/lib/main/widgets/bottom_nav.dart
@@ -0,0 +1,36 @@
+import 'package:cat_app/core/const';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controller/main_menu_controller.dart';
+
+// ignore: use_key_in_widget_constructors
+class BottomNavBarWidget extends StatelessWidget {
+  // Mengambil instance MainMenuController
+  final MainMenuController controller = Get.find<MainMenuController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => BottomNavigationBar(
+        currentIndex: controller.currentIndex.value,
+        onTap: controller.changePage,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: "Favorite",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.how_to_vote),
+            label: "Vote",
+          ),
+        ],
+        selectedItemColor: Colors.blue, // Warna untuk item aktif
+        unselectedItemColor: Colors.brown, // Warna untuk item tidak aktif
+      ),
+    );
+  }
+}
